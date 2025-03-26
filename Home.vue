91227f80c73fd24e29dd5c5efb357c77128d2aaa
ShangJunLong <1472912024@qq.com>
2025-03-26 08:40:02 +0800

    美食栏位

--- Home.vue
+++ Home.vue
@@ -14,11 +14,6 @@
         <line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line>
         <line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line>
       </svg>
-      <!--      <svg v-else xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-                 stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon">
-              <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
-            </svg >-->
-
       <svg v-else class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="3759"
            width="256" height="256">
         <path
@@ -100,6 +95,7 @@
             <button class="tab-btn active">最新</button>
             <button class="tab-btn">技术</button>
             <button class="tab-btn">生活</button>
+            <button class="tab-btn">美食</button>
             <button class="tab-btn">随想</button>
             <button class="tab-btn">测评</button>
           </div>
