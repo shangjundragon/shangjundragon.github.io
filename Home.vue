a21e4444768045b11cd0306b9d12f30f4e0d094e
ShangJunLong <1472912024@qq.com>
2025-03-26 09:57:57 +0800

    在 Settings → Pages 中，将 Source 设置为 GitHub Actions，而非分支，避免重复

--- Home.vue
+++ Home.vue
@@ -134,6 +134,7 @@ function handleClickTab(index) {
 }
 
 const activeTabContentList = ref([
+  {title: '在 Settings → Pages 中，将 Source 设置为 GitHub Actions，而非分支，避免重复', date: '2025.03.26'},
   {title: 'github的actions到底什么鬼？', date: '2025.03.26'},
   {title: '使用node加puppeteer构建生成pdf的docker镜像', date: '2025.03.20'},
   {title: '搭建个人博客的一个想法', date: '2025.03.06'},
