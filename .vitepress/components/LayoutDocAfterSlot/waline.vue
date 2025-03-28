8638b781f0295ed5575c1b7dbc0ca3c3d9a0b208
shangjunlong <1472912024@qq.com>
2025-03-28 19:33:02 +0800

    ok

--- .vitepress/components/LayoutDocAfterSlot/waline.vue
+++ .vitepress/components/LayoutDocAfterSlot/waline.vue
@@ -3,6 +3,8 @@
           :serverURL="serverURL"
           :path="docId"
           :dark="isDark"
+          :required-meta="['nick']"
+          reaction
   />
 </template>
 <script setup>
@@ -13,8 +15,12 @@ import {useData} from "vitepress";
 const docId = ref(undefined)
 const serverURL = 'https://vercel.driedrice.top';
 const {frontmatter, isDark} = useData();
-if (frontmatter.value.createDate) {
-  docId.value = frontmatter.value.createDate.replaceAll(' ', '').replaceAll(':', '').replaceAll('-', '');
-}
+onMounted(() => {
+  if (frontmatter.value.createDate) {
+    const hostname = location.hostname;
+    const dateId = frontmatter.value.createDate.replace(/[\s:-]/g, '');
+    docId.value = hostname === 'localhost' ? 'localhost' + dateId : dateId;
+  }
+})
 
 </script>
